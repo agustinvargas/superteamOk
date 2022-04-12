8c66caf9f7524d5079ec0f20b4504b6e542ae8af
Agustín Vargas <agustinvargas93@gmail.com>
2022-04-11 23:37:43 -0300

    upgrade insecure requests

--- src/utils/api/alkemy.js
+++ src/utils/api/alkemy.js
@@ -1,7 +1,7 @@
-import { crudder } from "./crudder";
+import { crudder } from './crudder';
 
-export const baseUrl = crudder("http://challenge-react.alkemy.org/");
+export const baseUrl = crudder('http://challenge-react.alkemy.org');
 export const alkemyData = {
-  email: "challenge@alkemy.org",
-  password: "react",
+  email: 'challenge@alkemy.org',
+  password: 'react',
 };
